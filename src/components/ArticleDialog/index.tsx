95140e6317cda5531010eead50e96069a9f8ea6f
Brian Farias <brianvicthorandp@gmail.com>
2024-07-16 08:53:53 -0300

    feat: starting the article cover logic

--- src/components/ArticleDialog/index.tsx
+++ src/components/ArticleDialog/index.tsx
@@ -1,43 +1,16 @@
+import { ArticleForm } from '../ArticleForm';
+import { Button } from '../ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog'
-import { Button } from '../ui/button'
-import { Input } from '../ui/input'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
-import { Textarea } from '../ui/textarea'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { useContext, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid';
-import { ArticlesContext } from '../../Contexts/ArticlesContext'
-
-const formSchema = z.object({
-  title: z.string().min(1),
-  content: z.string().min(1),
-  // cover: z.any(),
-})
+import { useState } from 'react'
 
 
 export function AritcleDialog() {
-  const articleContext = useContext(ArticlesContext);
+
   const [open, setOpen] = useState(false);
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      // cover: "",
-      content: ""
-    }
-  })
 
-  function addArticle(article: z.infer<typeof formSchema>) {
-    const { title, content } = article;
-    const newContent = content.replace(/\n/gi, '  \n');
-    articleContext!.addArticle({ id: uuidv4(), title, content: newContent });
-    form.setValue("title", "");
-    form.setValue("content", "");
-    setOpen(false);
-  }
+
+
 
   return (
 
@@ -59,68 +32,7 @@ export function AritcleDialog() {
             and remove your data from our servers.
           </DialogDescription>
         </DialogHeader>
-        <Form {...form} >
-          <form
-            onSubmit={form.handleSubmit((data) => {
-              addArticle(data)
-            })}
-            className='flex flex-col gap-3'
-          >
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Title</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Ex: How to center a div" className='mb-4' {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )
-              }}
-            />
-            <FormField
-              control={form.control}
-              name="content"
-              render={({ field }) => (
-                <FormItem className=''>
-                  <FormLabel>Content</FormLabel>
-                  <FormControl>
-                    <Textarea placeholder="Your article goes here!" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-
-
-              )}
-            />
-
-            {/* <FormField
-              control={form.control}
-              name="cover"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Cover</FormLabel>
-                    <FormControl>
-                      <Input {...field} type='file' accept='image/*' />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )
-              }}
-            /> */}
-
-
-            <Button type="submit" variant={'default'} className={`bg-green-600 hover:bg-green-650 focus:bg-green-700 hover:scale-105 focus:scale-105`} >Save Article</Button>
-
-
-          </form>
-
-        </Form>
-
+        <ArticleForm setOpen={setOpen} />
 
       </DialogContent>
     </Dialog >
---
+++ src/components/ArticleForm/index.tsx
@@ -0,0 +1,129 @@
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Textarea } from '../ui/textarea'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { useContext } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { ArticlesContext } from '../../Contexts/ArticlesContext'
+
+const formSchema = z.object({
+  title: z.string().min(1),
+  content: z.string().min(1),
+  cover: z.instanceof(FileList),
+})
+
+
+interface ArticleFormProps {
+  setOpen?: (open: boolean) => void
+}
+
+
+export function ArticleForm(props: ArticleFormProps) {
+
+  const articleContext = useContext(ArticlesContext);
+
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      cover: undefined,
+      content: ""
+    }
+  })
+
+  function handleArticleSubmit(article: z.infer<typeof formSchema>) {
+    const { title, content, cover } = article;
+    console.log({ title, content, cover });
+    const newContent = content.replace(/\n/gi, '  \n');
+    articleContext!.addArticle({ id: uuidv4(), title, content: newContent });
+    form.setValue("title", "");
+    form.setValue("content", "");
+    if (props?.setOpen) {
+      props.setOpen(false);
+    }
+  }
+
+  return (
+    <Form {...form} >
+      <form
+        onSubmit={form.handleSubmit((data) => {
+          console.log(data)
+          handleArticleSubmit(data)
+        })}
+        className='flex flex-col gap-3'
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Ex: How to center a div" className='mb-4' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )
+          }}
+        />
+
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem className=''>
+              <FormLabel>Content</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Your article goes here!" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+
+          )}
+        />
+        {/* Input type="file" has to be uncontrolled, hence I'm using this structure, instead of the shadcn FormFiel */}
+        <Input
+          type="file"
+          accept="img/*"
+          {...form.register("cover")}
+          onChange={(event) => {
+            const cover = event.target.files ? event.target.files[0] : null;
+            return cover;
+          }}
+        />
+
+        {form.formState.errors?.cover && <p>{form.formState.errors.cover.message} </p>}
+
+        {/* <FormField
+          control={form.control}
+          name="cover"
+          render={({ field: { onChange, ...fieldProps } }) => (
+            <FormItem>
+              <FormLabel>Cover</FormLabel>
+              <FormControl>
+                
+                <Input
+                  type="file"
+                  accept="img/*"
+                  onChange={(event) =>
+                    onChange(event.target.files && event.target.files[0])
+                  }
+                  {...fieldProps} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */ }
+
+        <Button type="submit" variant={'default'} className={`bg-green-600 hover:bg-green-650 focus:bg-green-700 hover:scale-105 focus:scale-105`} >Save Article</Button>
+
+      </form>
+
+    </Form>
+  )
+}
